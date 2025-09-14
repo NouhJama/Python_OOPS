0ec6ce6f6c7e6fa11ba3b0f9e7dd78433e86c54b
Nouh <noohahmed600@gmail.com>
2025-09-14 12:53:27 +0300

    Created an instance of the student and updated the Student class, added the constructor init function

--- student5.py
+++ student5.py
@@ -1,18 +1,20 @@
 class Student:
-    ... # IGNORE
+    def __init__(self, name, house): # Constructor to initialize name and house
+        self.name = name # Instance variable for name
+        self.house = house # Instance variable for house
 
 
 def main():
-    student = get_student()
+    student = get_student() # Get an instance of Student
+    if student.name == "Padma": # Access name using dot notation
+        student.house = "Ravenclaw" # Modify house using dot notation   
     print(f"{student.name} from {student.house}")
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    student = Student()
-    student.name = name
-    student.house = house   
-    return student
+    student = Student(name, house) # Create an instance of Student
+    return student # Return the instance
 
 if __name__ == "__main__":
     main()
